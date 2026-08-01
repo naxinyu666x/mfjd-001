4b2ceda38990e57742c32a2cc0ab8c12e6ec3fb0
naxinyu666x <maxinyu666x@protonmail.com>
2026-08-01 21:53:59 +0800

    Update api.js

--- css/js/js/api.js
+++ css/js/js/api.js
@@ -1,1 +1,21 @@
-const API_BASE = "https://mfjd-001.aly16888.workers.dev";
+
+async function checkWorker() {
+    try {
+        const response = await fetch(API_BASE + "/hello");
+
+        const data = await response.json();
+
+        console.log(data);
+
+        alert(
+            "Worker 正常\n\n" +
+            JSON.stringify(data, null, 2)
+        );
+
+    } catch (err) {
+
+        console.error(err);
+
+        alert("Worker 无法连接");
+    }
+}
